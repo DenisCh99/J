358b7be03eb5716a5de0c467df8df9f7ff675601
DenisCh99 <daygishev@mail.ru>
2023-10-26 17:58:11 +0300

    lesson 8

--- src/main/java/Lesson__8/CounterApp.java
+++ src/main/java/Lesson__8/CounterApp.java
@@ -46,6 +46,7 @@ public class CounterApp extends JFrame {
             }
         });
 
+
         incrementButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
